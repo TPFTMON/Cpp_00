7285e2111fc5f2bf3cf0bee375440557a221512f
TPFTMON <tpftmoon@gmail.com>
2025-12-12 18:11:20 +0100

    PhoneBook almost implemented

---
+++ ex01/PhoneBook.cpp
@@ -0,0 +1,36 @@
+#include "PhoneBook.hpp"
+
+PhoneBook::PhoneBook(){
+}
+
+PhoneBook::~PhoneBook(){
+}
+
+void PhoneBook::addContact(int index){
+    std::string prompts[5] = {
+        "Enter First Name:",
+        "Enter Last Name:",
+        "Enter Nickname",
+        "Enter Phone Number:",
+        "Enter Darkest Secret:"
+    };
+
+    Contact NewContact;
+    std::string input;
+
+    for (size_t i = 0; i < 5; i++){
+        std::cout << prompts[i];
+
+        if (!std::getline(std::cin, input))
+            break;
+
+        if (input.empty()){
+            std::cout << "Field cannot be empty. Write something!";
+            i--;
+            continue;
+        }
+        NewContact.SetInfo(i, input);
+    }
+
+    // ... Save into the PhoneBook logic:
+}
--- ex01/PhoneBook.hpp
+++ ex01/PhoneBook.hpp
@@ -1,4 +1,21 @@
 #ifndef PHONEBOOK_HPP
+# define PHONEBOOK_HPP
+
+# include <string>
+# include <iostream>
+# include "Contact.hpp"
+
+class PhoneBook{
+
+private:
+    Contact Contacts[8];
+
+public:
+    PhoneBook();
+    ~PhoneBook();
+    void addContact(int index);
+};
+
 
 
 
